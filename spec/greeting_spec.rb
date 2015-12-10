4d29cd5ed1854c0cb4a856c4d0d9442dad48f30c
Tim Hyson <tjhyson@gmail.com>
2015-12-10 23:29:01 +0000

    updates test to use latest rspec syntax to stub user input

--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -3,7 +3,7 @@ require 'greeting'
 describe '#say_hello' do
   it "greets a user by name" do
     greeting = Greeting.new
-    greeting.stub(:gets) {'Tim'}
+    allow(greeting).to receive(:gets).and_return('tim')
     expect(greeting.say_hello).to eq('Hello, Tim, nice to meet you!')
   end
 end
